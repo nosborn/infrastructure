13bc556c3059468eecd899a873a727415e458306
Nick Osborn <nosborn@users.noreply.github.com>
2018-01-06 23:59:45 +0000

    Unpause monitoring

--- cannoli/monitoring.tf
+++ cannoli/monitoring.tf
@@ -1,17 +1,8 @@
-# TODO:
-#resource "statuscake_test" "DNS" {
-#  website_name  = "cannoli.london"
-#  website_url   = "cannoli.london"
-#  test_type     = "DNS"
-#  confirmations = 2
-#}
-
 resource "statuscake_test" "HTTP" {
   website_name  = "cannoli.london"
   website_url   = "https://cannoli.london/ping.txt"
-  check_rate    = 1200
+  check_rate    = 1800
   test_type     = "HTTP"
-  paused        = true
   confirmations = 2
 
   depends_on = [
