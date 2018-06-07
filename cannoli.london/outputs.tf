b5a04ecd8c5df8103b4b1ccb6090f6ea47c24c28
Nick Osborn <nosborn@users.noreply.github.com>
2018-06-07 22:30:27 +0100

    Add outputs to cannoli.london

--- cannoli.london/outputs.tf
+++ cannoli.london/outputs.tf
@@ -1,3 +1,11 @@
+output "cloudfront_distribution_id" {
+  value = "${aws_cloudfront_distribution.main.id}"
+}
+
+output "content_bucket_id" {
+  value = "${aws_s3_bucket.content.id}"
+}
+
 output "deploy_access_key_id" {
   value = "${aws_iam_access_key.deploy.id}"
 }
