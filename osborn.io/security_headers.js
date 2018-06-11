093a9915fe2dbfb5803736aac7fa8b83eefd6ec5
Nick Osborn <nosborn@users.noreply.github.com>
2018-06-11 06:02:03 +0100

    Update subdomain for Report URI

--- osborn.io/security_headers.js
+++ osborn.io/security_headers.js
@@ -6,8 +6,8 @@ exports.handler = (event, context, callback) => {
   const headers = response.headers;
 
   // Set new headers 
-  headers['content-security-policy'] = [{key: 'Content-Security-Policy', value: "default-src 'none'; style-src 'self'; upgrade-insecure-requests; referrer origin; report-uri https://tombstone.report-uri.com/r/d/csp/enforce"}]; 
-  headers['expect-ct'] = [{key: 'Expect-CT', value: 'max-age=60; report-uri="https://tombstone.report-uri.com/r/d/ct/enforce; enforce'}]; 
+  headers['content-security-policy'] = [{key: 'Content-Security-Policy', value: "default-src 'none'; style-src 'self'; upgrade-insecure-requests; referrer origin; report-uri https://nosborn.report-uri.com/r/d/csp/enforce"}]; 
+  headers['expect-ct'] = [{key: 'Expect-CT', value: 'max-age=60; report-uri="https://nosborn.report-uri.com/r/d/ct/enforce; enforce'}]; 
   headers['referrer-policy'] = [{key: 'Referrer-Policy', value: 'same-origin'}]; 
   headers['strict-transport-security'] = [{key: 'Strict-Transport-Security', value: 'max-age=31536000; includeSubdomains; preload'}]; 
   headers['x-content-type-options'] = [{key: 'X-Content-Type-Options', value: 'nosniff'}]; 
