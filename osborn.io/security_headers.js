5784497bf497673d04d28e4cffc769c15789720c
Nick Osborn <nosborn@users.noreply.github.com>
2018-06-13 06:39:17 +0100

    Update security headers

--- osborn.io/security_headers.js
+++ osborn.io/security_headers.js
@@ -7,7 +7,7 @@ exports.handler = (event, context, callback) => {
 
   // Set new headers 
   headers['content-security-policy'] = [{key: 'Content-Security-Policy', value: "default-src 'none'; style-src 'self'; upgrade-insecure-requests; referrer origin; report-uri https://nosborn.report-uri.com/r/d/csp/enforce"}]; 
-  headers['expect-ct'] = [{key: 'Expect-CT', value: 'max-age=86400; report-uri="https://nosborn.report-uri.com/r/d/ct/enforce; enforce'}]; 
+  headers['expect-ct'] = [{key: 'Expect-CT', value: 'report-uri="https://nosborn.report-uri.com/r/d/ct/enforce", enforce, max-age=86400'}]; 
   headers['referrer-policy'] = [{key: 'Referrer-Policy', value: 'same-origin'}]; 
   headers['strict-transport-security'] = [{key: 'Strict-Transport-Security', value: 'max-age=63072000; includeSubdomains; preload'}]; 
   headers['x-content-type-options'] = [{key: 'X-Content-Type-Options', value: 'nosniff'}]; 
