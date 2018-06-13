becb7b0598462161e8cbf87a37341e6a20dc1249
Nick Osborn <nosborn@users.noreply.github.com>
2018-06-13 06:39:17 +0100

    Update security headers

--- cannoli.london/security_headers.js
+++ cannoli.london/security_headers.js
@@ -6,7 +6,7 @@ exports.handler = (event, context, callback) => {
   const headers = response.headers;
 
   // Set new headers 
-  headers['expect-ct'] = [{key: 'Expect-CT', value: 'max-age=86400; report-uri="https://nosborn.report-uri.com/r/d/ct/enforce; enforce'}]; 
+  headers['expect-ct'] = [{key: 'Expect-CT', value: 'report-uri="https://nosborn.report-uri.com/r/d/ct/enforce", enforce, max-age=86400'}]; 
   headers['referrer-policy'] = [{key: 'Referrer-Policy', value: 'same-origin'}]; 
   headers['strict-transport-security'] = [{key: 'Strict-Transport-Security', value: 'max-age=63072000; includeSubdomains; preload'}]; 
   headers['x-content-type-options'] = [{key: 'X-Content-Type-Options', value: 'nosniff'}]; 
