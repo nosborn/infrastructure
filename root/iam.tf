a3109573a785675a105b735efab84c646b6c0a0d
Nick Osborn <nosborn@users.noreply.github.com>
2018-06-13 18:24:16 +0100

    Set account password policy

--- root/iam.tf
+++ root/iam.tf
@@ -1,3 +1,12 @@
 resource "aws_iam_account_alias" "main" {
   account_alias = "${var.account_alias}"
 }
+
+resource "aws_iam_account_password_policy" "main" {
+  allow_users_to_change_password = true
+  minimum_password_length        = 12
+  require_lowercase_characters   = true
+  require_numbers                = true
+  require_uppercase_characters   = true
+  require_symbols                = true
+}
