d0ffebf4a84e22ab0f43fd68ba773e489d6d3516
Nick Osborn <nosborn@users.noreply.github.com>
2018-06-14 07:32:42 +0100

    Add StatusCake provider

--- osborn.io/gitlab.tf
+++ osborn.io/gitlab.tf
@@ -1,6 +1,6 @@
 resource "gitlab_project" "main" {
   name                   = "osborn.io"
-  description            = ""
+  description            = "Source for [https://osborn.io]."
   default_branch         = "master"
   issues_enabled         = true
   merge_requests_enabled = false
