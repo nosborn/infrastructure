86b8c41662b88fd2fdd9639a068145d3fa7b9e8c
Nick Osborn <nosborn@users.noreply.github.com>
2018-06-14 08:12:27 +0100

    Turn off GitLab issues

--- osborn.io/gitlab.tf
+++ osborn.io/gitlab.tf
@@ -2,7 +2,7 @@ resource "gitlab_project" "main" {
   name                   = "osborn.io"
   description            = "Source for [https://osborn.io]."
   default_branch         = "master"
-  issues_enabled         = true
+  issues_enabled         = false
   merge_requests_enabled = false
   wiki_enabled           = false
   snippets_enabled       = false
