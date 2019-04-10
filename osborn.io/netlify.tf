9bca1638fabac93dc4d765476f4c9fbddd33a228
Nick Osborn <nosborn@users.noreply.github.com>
2019-04-10 12:31:32 +0800

    Rename osborn.io -> www.osborn.io

--- osborn.io/netlify.tf
+++ osborn.io/netlify.tf
@@ -1,10 +1,10 @@
 resource "netlify_site" "main" {
-  name          = "${replace(var.domain_name, ".", "-")}"
+  name          = "www-${replace(var.domain_name, ".", "-")}"
   custom_domain = "www.${var.domain_name}"
 
   repo {
     provider = "github"
-    repo_path = "${var.github_username}/${var.domain_name}"
+    repo_path = "${var.github_username}/www.${var.domain_name}"
     repo_branch = "master"
   }
 }
