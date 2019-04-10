5c843e8b0651458f1457a534644541ae943d422f
Nick Osborn <nosborn@users.noreply.github.com>
2019-04-10 12:13:59 +0800

    Migrate www.osborn.io to Netlify

--- osborn.io/outputs.tf
+++ osborn.io/outputs.tf
@@ -1,18 +1,10 @@
-output "cloudfront_distribution_id" {
-  value = "${aws_cloudfront_distribution.main.id}"
-}
+#output "certbot_access_key_id" {
+#  value = "${aws_iam_access_key.certbot.id}"
+#}
 
-output "content_bucket_id" {
-  value = "${aws_s3_bucket.content.id}"
-}
-
-output "deploy_access_key_id" {
-  value = "${aws_iam_access_key.deploy.id}"
-}
-
-output "deploy_secret_access_key" {
-  value = "${aws_iam_access_key.deploy.secret}"
-}
+#output "certbot_secret_access_key" {
+#  value = "${aws_iam_access_key.certbot.secret}"
+#}
 
 output "dyndns_access_key_id" {
   value = "${aws_iam_access_key.dyndns.id}"
