c7ee490ccca06a77e785f4ea3587632709594c9d
Nick Osborn <nosborn@users.noreply.github.com>
2019-04-29 13:05:02 +0800

    terraform fmt

--- osborn.io/netlify.tf
+++ osborn.io/netlify.tf
@@ -3,8 +3,8 @@ resource "netlify_site" "main" {
   custom_domain = "${var.domain_name}"
 
   repo {
-    provider = "github"
-    repo_path = "${var.github_username}/${var.domain_name}"
+    provider    = "github"
+    repo_path   = "${var.github_username}/${var.domain_name}"
     repo_branch = "master"
   }
 }
