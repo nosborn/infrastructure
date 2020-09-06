2323626e03e4ebef76d27892ad7cdad5651e08e5
Nick Osborn <nosborn@users.noreply.github.com>
2020-09-06 16:42:43 +0800

    Remove luctam.com website

--- luctam.com/outputs.tf
+++ luctam.com/outputs.tf
@@ -1,11 +1,3 @@
-output "deploy_access_key_id" {
-  value = aws_iam_access_key.deploy.id
-}
-
-output "deploy_secret_access_key" {
-  value = aws_iam_access_key.deploy.secret
-}
-
 output "name_servers" {
   value       = cloudflare_zone.main.name_servers
   description = "Cloudflare-assigned name servers."
