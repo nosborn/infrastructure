702b1ad2c61785311b65871f3e1d9b51defe145d
Nick Osborn <nosborn@users.noreply.github.com>
2022-01-18 12:24:34 +0800

    Tell Dependabot about Terraform

--- luctam.com/versions.tf
+++ luctam.com/versions.tf
@@ -4,12 +4,12 @@ terraform {
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = "~> 3.70"
+      version = "3.70.0"
     }
 
     cloudflare = {
       source  = "cloudflare/cloudflare"
-      version = "~> 3.5"
+      version = "3.5.0"
     }
   }
 }
