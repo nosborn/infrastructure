9a2fc8f84d64801ace71059a696902459f351fa4
Nick Osborn <nosborn@users.noreply.github.com>
2022-03-03 23:18:12 +0800

    Update IAM password policy

--- iam.tf
+++ iam.tf
@@ -2,9 +2,11 @@ resource "aws_iam_account_alias" "main" {
   account_alias = var.account_alias
 }
 
+# tfsec:ignore:aws-iam-set-max-password-age
 resource "aws_iam_account_password_policy" "main" {
   allow_users_to_change_password = true
-  minimum_password_length        = 12
+  minimum_password_length        = 14
+  password_reuse_prevention      = 10
   require_lowercase_characters   = true
   require_numbers                = true
   require_uppercase_characters   = true
