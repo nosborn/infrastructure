b1619c3666c131bbb9ba77a1f424fa7e31a1b70c
Nick Osborn <nosborn@users.noreply.github.com>
2022-05-31 06:59:03 +0800

    Yay, Dependabot secrets

--- github.tf
+++ github.tf
@@ -3,3 +3,9 @@ resource "github_actions_secret" "aws_role_to_assume" {
   secret_name     = "AWS_ROLE_TO_ASSUME"
   plaintext_value = aws_iam_role.github_actions_workflow.arn
 }
+
+resource "github_dependabot_secret" "aws_role_to_assume" {
+  repository      = "infrastructure"
+  secret_name     = "AWS_ROLE_TO_ASSUME"
+  plaintext_value = aws_iam_role.github_actions_workflow.arn
+}
