2adf202b393b8738e823bc1ecf2cefdfd98caa39
Nick Osborn <nosborn@users.noreply.github.com>
2024-06-28 05:05:56 +0800

    chore: Terraform 1.9

--- modules/static-website/versions.tf
+++ modules/static-website/versions.tf
@@ -1,9 +1,12 @@
 terraform {
   required_providers {
     aws = {
-      source                = "hashicorp/aws"
-      version               = ">= 4.23"
-      configuration_aliases = [aws.us_east_1]
+      source  = "hashicorp/aws"
+      version = ">= 4.23"
+
+      configuration_aliases = [
+        aws.us_east_1,
+      ]
     }
 
     random = {
