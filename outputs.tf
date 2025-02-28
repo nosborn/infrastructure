cd79f87891a8db1c83b16ab8953dec60af891313
Nick Osborn <nosborn@users.noreply.github.com>
2025-02-28 14:03:38 +0800

    Remove unused outputs

--- outputs.tf
+++ outputs.tf
@@ -1,22 +1,10 @@
-output "dns_updater_scaleway_access_key" {
-  description = "The access Key of the IAM API key."
-  value       = scaleway_iam_api_key.dns_updater.access_key
-  sensitive   = true
-}
-
-output "dns_updater_scaleway_secret_key" {
+output "dns_updater_scaleway_api_token" {
   description = "The secret Key of the IAM API key."
   value       = scaleway_iam_api_key.dns_updater.secret_key
   sensitive   = true
 }
 
-output "lego_scaleway_access_key" {
-  description = "The access Key of the IAM API key."
-  value       = scaleway_iam_api_key.lego.access_key
-  sensitive   = true
-}
-
-output "lego_scaleway_secret_key" {
+output "lego_scaleway_api_token" {
   description = "The secret Key of the IAM API key."
   value       = scaleway_iam_api_key.lego.secret_key
   sensitive   = true
