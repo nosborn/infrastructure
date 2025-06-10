c7f922a6d162b3f2591d7124a0b3bee580e07863
Nick Osborn <nosborn@users.noreply.github.com>
2025-06-10 08:43:07 +0800

    chore(providers): scaleway/scaleway 2.55.0

--- modules/osborn-ws/versions.tf
+++ modules/osborn-ws/versions.tf
@@ -2,7 +2,7 @@ terraform { # tflint-ignore: terraform_required_version
   required_providers {
     scaleway = {
       source  = "scaleway/scaleway"
-      version = "2.54.0"
+      version = "2.55.0"
     }
   }
 }
--- versions.tf
+++ versions.tf
@@ -14,7 +14,7 @@ terraform {
 
     scaleway = {
       source  = "scaleway/scaleway"
-      version = "2.54.0"
+      version = "2.55.0"
     }
 
     time = {
