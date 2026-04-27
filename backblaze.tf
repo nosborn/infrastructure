f0141693168485b8596a59b4449b3134edf9b4f6
Nick Osborn <nosborn@users.noreply.github.com>
2026-04-27 11:42:12 +0100

    Set up HyperBackup

--- backblaze.tf
+++ backblaze.tf
@@ -1,35 +1,3 @@
-resource "b2_bucket" "cloudsync" {
-  bucket_name = "io-osborn-cloudsync"
-  bucket_type = "allPrivate"
-
-  default_server_side_encryption {
-    algorithm = "AES256"
-    mode      = "SSE-B2"
-  }
-
-  lifecycle_rules {
-    file_name_prefix             = ""
-    days_from_hiding_to_deleting = 30
-  }
-}
-
-resource "b2_application_key" "cloudsync" {
-  key_name = "CloudSync"
-
-  bucket_ids = [
-    b2_bucket.cloudsync.id,
-  ]
-
-  capabilities = [
-    "deleteFiles",
-    "listBuckets",
-    "listFiles",
-    "readBuckets",
-    "readFiles",
-    "writeFiles",
-  ]
-}
-
 resource "b2_bucket" "diskstation" {
   bucket_name = "io-osborn-diskstation"
   bucket_type = "allPrivate"
@@ -45,8 +13,18 @@ resource "b2_bucket" "diskstation" {
   }
 }
 
-resource "b2_application_key" "hyperbackup" {
-  key_name = "HyperBackup"
+resource "b2_bucket" "hyperbackup" {
+  bucket_name = "io-osborn-hyperbackup"
+  bucket_type = "allPrivate"
+
+  lifecycle_rules {
+    file_name_prefix             = ""
+    days_from_hiding_to_deleting = 1 # "Keep only the last version"
+  }
+}
+
+resource "b2_application_key" "cloudsync" {
+  key_name = "CloudSync"
 
   bucket_ids = [
     b2_bucket.diskstation.id,
@@ -54,6 +32,24 @@ resource "b2_application_key" "hyperbackup" {
 
   capabilities = [
     "deleteFiles",
+    "listBuckets",
+    "listFiles",
+    "readBuckets",
+    "readFiles",
+    "writeFiles",
+  ]
+}
+
+resource "b2_application_key" "hyperbackup" {
+  key_name = "HyperBackup"
+
+  bucket_ids = [
+    b2_bucket.hyperbackup.id,
+  ]
+
+  capabilities = [
+    "deleteFiles",
+    "listAllBucketNames",
     "listBuckets",
     "listFiles",
     "readBuckets",
